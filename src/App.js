b27bce2c454a100644afef88bdfb8ac891759997
MARCUS <cg3011631@LAB204M09.IFSP>
2022-08-30 21:08:58 -0300

    Criando o banner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
+import tmdb from './tmdb'
+import Header from './components/header'
+import FeaturedMovie from './components/FeaturedMovie'
 
-function App() {
+export default() => 
+{
+  const [movieList, setMovieList] = useState([]);
+
+  const [featuredData, setFeaturedData] = useState(null); 
+  const [blackHeader, setBlackHeader] = useState('false')
+  useEffect(() => {
+    const loadAll = async() => {
+      let list =  await tmdb.getHomeList();
+      console.log(list)
+      setMovieList(list)
+      
+      let originals = list.filter(i => i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await tmdb.getMovieInfo(chosen.id, 'tv')
+      setFeaturedData(chosenInfo)
+    }
+    loadAll()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="page">
+      <Header black = {blackHeader}/>
 
-export default App;
+      {featuredData && 
+        <FeaturedMovie item={featuredData}/>
+      }
+    </div>
+  )
+}
